a4daf58e77611975c4280261ae4284d3c3de4892
StillPointOS <stillpointappteam@gmail.com>
2026-06-29 11:17:22 -0400

    Delete app directory

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Page() {
-  return (
-    <div style={{ padding: "40px", fontFamily: "sans-serif" }}>
-      <h1>StillPointOS</h1>
-      <p>Your homepage is now live.</p>
-    </div>
-  );
-}
