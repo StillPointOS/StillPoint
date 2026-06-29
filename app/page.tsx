f2865bb11aecbf93b1428cfbb3cf6a72bff965e1
StillPointOS <stillpointappteam@gmail.com>
2026-06-29 11:32:41 -0400

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div style={{ padding: "40px", fontFamily: "sans-serif" }}>
+      <h1>StillPointOS</h1>
+      <p>Your homepage is now live.</p>
+    </div>
+  );
+}
